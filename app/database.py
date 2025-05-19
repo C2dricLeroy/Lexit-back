2fab241f5446a66490d60c43babcfd628a1f0bb4
celer <cedric.leroy@smile.fr>
2025-05-19 13:51:34 +0200

    [INIT] db connection + [FIX] dependencies

--- app/database.py
+++ app/database.py
@@ -1,7 +1,23 @@
-from sqlmodel import SQLModel, create_engine
+from sqlmodel import SQLModel, create_engine, Session
+from typing import Generator
+from logging import INFO, basicConfig, getLogger
 from app.config import settings
+
+log = getLogger(__name__)
+basicConfig(level=INFO)
 
 engine = create_engine(settings.database_url, echo=settings.DEBUG)
 
 def init_db():
     SQLModel.metadata.create_all(engine)
+
+def get_session() -> Generator[Session, Session, None]:
+    """
+    Generator for a database session to be used in routers.
+
+    Return: Database session generator
+    """
+
+    log.info("Initialising database session...")
+    with Session(engine) as session:
+        yield session
--- app/main.py
+++ app/main.py
@@ -2,14 +2,28 @@ from fastapi import FastAPI
 from app.config import settings
 from contextlib import asynccontextmanager
 from app.database import init_db
+from logging import INFO, basicConfig, getLogger
 
-app = FastAPI(title=f"{settings.APP_NAME} ({settings.ENV})", debug=settings.DEBUG)
+def get_app() -> FastAPI:
+    app = FastAPI(title=f"{settings.APP_NAME} ({settings.ENV})", debug=settings.DEBUG)
+    # app.include_router(product.router)
+    return app
+
+FastAPI(title=f"{settings.APP_NAME} ({settings.ENV})", debug=settings.DEBUG)
+
+logger = getLogger(__name__)
+basicConfig(level=INFO)
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
+    logger.info("Starting up...")
     init_db()
     yield
+    logger.info("Shutting down...")
+    logger.info("Finished shutting down.")
+
+app = get_app()
 
 
 @app.get("/")
