521bea13ecfaf6608d32496c48a54beabc9d0002
celer <cedric.leroy@smile.fr>
2025-05-23 15:04:30 +0200

    [IMP] post method

--- app/routes/language.py
+++ app/routes/language.py
@@ -1,10 +1,28 @@
 from fastapi import APIRouter, Depends
 from sqlmodel import Session, select
-from app.models.language import Language
+
 from app.database import get_session
+from app.models.language import Language
 
 router = APIRouter()
 
+
 @router.get("/")
 def get_languages(session: Session = Depends(get_session)):
+    """Return all languages."""
     return session.exec(select(Language)).all()
+
+
+@router.get("/{id}")
+def get_language_by_id(id: int, session: Session = Depends(get_session)):
+    """Return a language by its ID."""
+    return session.exec(select(Language).where(Language.id == id)).first()
+
+
+@router.post("/")
+def create_language(language: Language, session: Session = Depends(get_session)):
+    """Create a new language."""
+    session.add(language)
+    session.commit()
+    session.refresh(language)
+    return language
