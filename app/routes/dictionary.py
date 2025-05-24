10b97ac9293edeeaf87e955308208f13731c765f
C2dricLeroy <pro.cedricleroy@gmail.com>
2025-05-24 18:07:39 +0200

    [IMP] dictionary creation with user integration

--- app/routes/dictionary.py
+++ app/routes/dictionary.py
@@ -1,11 +1,13 @@
 from logging import getLogger
 
 from fastapi import APIRouter, Depends
+from fastapi.exceptions import HTTPException
 from sqlmodel import Session, select
 
 from app.database import get_session
-from app.dto.dictionary import DictionaryRead
+from app.dto.dictionary import DictionaryCreate, DictionaryRead
 from app.models.dictionary import Dictionary
+from app.models.user import User
 
 router = APIRouter()
 _logger = getLogger(__name__)
@@ -25,3 +27,21 @@ def get_dictionary_by_id(
     return session.exec(
         select(Dictionary).where(Dictionary.id == dictionary_id)
     ).first()
+
+
+@router.post("/", response_model=DictionaryRead, status_code=201)
+def create_dictionary(
+    dictionary: DictionaryCreate, session: Session = Depends(get_session)
+):
+    """Create a new dictionary."""
+    db_user = session.get(User, dictionary.user_id)
+    if not db_user:
+        raise HTTPException(status_code=404, detail="User not found")
+
+    db_dictionary = Dictionary(**dictionary.model_dump())
+    db_user.dictionaries.append(db_dictionary)  # Facultatif mais explicite
+
+    session.add(db_dictionary)
+    session.commit()
+    session.refresh(db_dictionary)
+    return db_dictionary
