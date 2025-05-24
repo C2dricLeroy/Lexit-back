60b4f3ce7311c748361f5e98deb27e30d42850fc
C2dricLeroy <pro.cedricleroy@gmail.com>
2025-05-24 10:47:20 +0200

    [CLEAN] main + architecture [IMP] DTO on language

--- app/routes/language.py
+++ app/routes/language.py
@@ -2,27 +2,35 @@ from fastapi import APIRouter, Depends
 from sqlmodel import Session, select
 
 from app.database import get_session
+from app.dto.language import LanguageCreate, LanguageRead
 from app.models.language import Language
 
 router = APIRouter()
 
 
-@router.get("/")
+@router.get("/", response_model=list[LanguageRead])
 def get_languages(session: Session = Depends(get_session)):
     """Return all languages."""
     return session.exec(select(Language)).all()
 
 
-@router.get("/{id}")
-def get_language_by_id(id: int, session: Session = Depends(get_session)):
+@router.get("/{id}", response_model=list[LanguageRead])
+def get_language_by_id(
+    language_id: int, session: Session = Depends(get_session)
+):
     """Return a language by its ID."""
-    return session.exec(select(Language).where(Language.id == id)).first()
+    return session.exec(
+        select(Language).where(Language.id == language_id)
+    ).first()
 
 
-@router.post("/")
-def create_language(language: Language, session: Session = Depends(get_session)):
+@router.post("/", response_model=LanguageRead, status_code=201)
+def create_language(
+    language: LanguageCreate, session: Session = Depends(get_session)
+):
     """Create a new language."""
-    session.add(language)
+    db_language = Language(**language.model_dump())
+    session.add(db_language)
     session.commit()
-    session.refresh(language)
-    return language
+    session.refresh(db_language)
+    return db_language
