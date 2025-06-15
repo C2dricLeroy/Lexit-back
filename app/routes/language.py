eb6c5886aa5cb6ccb48692f08ba560de7da1be1e
C2dricLeroy <pro.cedricleroy@gmail.com>
2025-06-15 21:22:25 +0200

    [IMP] admin rights : language + user deletion

--- app/routes/language.py
+++ app/routes/language.py
@@ -1,6 +1,6 @@
 from typing import List
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlmodel import Session, select
 from starlette.requests import Request
 
@@ -8,6 +8,8 @@ from app.core.limiter import limiter
 from app.database import get_session
 from app.dto.language import LanguageCreate, LanguageRead
 from app.models.language import Language
+from app.models.user import User
+from app.services.user import get_current_user
 
 router = APIRouter()
 
@@ -43,3 +45,27 @@ def create_language(
     session.commit()
     session.refresh(db_language)
     return [db_language]
+
+
+@router.delete("/admin/{language_id}", response_model=List[LanguageRead])
+@limiter.limit("5/minute")
+def delete_language(
+    request: Request,
+    language_id: int,
+    current_user: User = Depends(get_current_user),
+    session: Session = Depends(get_session),
+):
+    """Admin delete a language by its ID."""
+    db_language = session.get(Language, language_id)
+    if not db_language:
+        raise HTTPException(status_code=404, detail="Language not found")
+
+    if not current_user.is_superuser:
+        raise HTTPException(
+            status_code=403,
+            detail="You are not authorized to delete this language.",
+        )
+
+    session.delete(db_language)
+    session.commit()
+    return {"message": "Language deleted successfully"}
