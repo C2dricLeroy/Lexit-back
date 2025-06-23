ab5bb26048663b73b74dd5802aa0dbdbc0020866
celer <cedric.leroy@smile.fr>
2025-06-23 15:29:57 +0200

    [FIX] logger

--- app/core/logger.py
+++ app/core/logger.py
@@ -2,26 +2,27 @@ import logging
 import os
 from logging.handlers import RotatingFileHandler
 
-LOG_PATH = os.path.expanduser("~/lexit_logs")
+LOG_PATH = os.path.expanduser("/var/log/lexit")
 os.makedirs(LOG_PATH, exist_ok=True)
 LOG_FILE = os.path.join(LOG_PATH, "app.log")
 
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
-
-handler = RotatingFileHandler(LOG_FILE, maxBytes=10_000_000, backupCount=5)
 formatter = logging.Formatter(
     "%(asctime)s - %(levelname)s - %(name)s - %(message)s"
 )
-handler.setFormatter(formatter)
 
-if not any(isinstance(h, RotatingFileHandler) for h in logger.handlers):
-    logger.addHandler(handler)
+file_handler = RotatingFileHandler(
+    LOG_FILE, maxBytes=10_000_000, backupCount=5
+)
+file_handler.setFormatter(formatter)
 
-uvicorn_logger = logging.getLogger("uvicorn")
-uvicorn_logger.handlers = [handler]
-uvicorn_logger.setLevel(logging.INFO)
+console_handler = logging.StreamHandler()
+console_handler.setFormatter(formatter)
 
-uvicorn_access_logger = logging.getLogger("uvicorn.access")
-uvicorn_access_logger.handlers = [handler]
-uvicorn_access_logger.setLevel(logging.INFO)
+for log_name in ("", "uvicorn", "uvicorn.access"):
+    log = logging.getLogger(log_name)
+    log.setLevel(logging.INFO)
+
+    if not any(isinstance(h, RotatingFileHandler) for h in log.handlers):
+        log.addHandler(file_handler)
+    if not any(isinstance(h, logging.StreamHandler) for h in log.handlers):
+        log.addHandler(console_handler)
